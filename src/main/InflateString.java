69c583cb1926e9d821371fa176e55054c107352f
Rodrigo Mateus <rodrigo.m.mateus@gmail.com>
2016-05-09 18:19:32 -0300

    Refatoração

--- src/main/InflateString.java
+++ src/main/InflateString.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Arrays;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 import java.io.IOException;
@@ -14,34 +15,38 @@ public class InflateString {
 		// TODO Auto-generated method stub
 		try {
 
-//			// Encode a String into bytes
-//			String inputString = "String compactada pela QuaZIP";
-//			byte[] input = inputString.getBytes("UTF-8");
-//			System.out.println("input: " + Hex.encodeHexString(input));
-//
-//			// Compress the bytes
-//			byte[] output = new byte[2048];
-//			Deflater compresser = new Deflater();
-//			compresser.setInput(input);
-//			compresser.finish();
-//			int compressedDataLength = compresser.deflate(output);
-//			System.out.println("output: " + Hex.encodeHexString(output));
+			// Encode a String into bytes
+			String inputString = "String compactada pela QuaZIP";
+			byte[] inputArray = inputString.getBytes("UTF-8");
+			System.out.println("input: " + Hex.encodeHexString(inputArray));
 
-			Path path = Paths.get("string_de_teste_compactada.qzip");
-			byte[] data = Files.readAllBytes(path);
-			System.out.println("data: " + Hex.encodeHexString(data));
+			// Compress the bytes
+			byte[] compresserOutput = new byte[2048];
+			Deflater compresser = new Deflater();
+			compresser.setInput(inputArray);
+			compresser.finish();
+			int compressedOutputLength = compresser.deflate(compresserOutput);
+			byte[] compactData = Arrays.copyOfRange(compresserOutput, 0, compressedOutputLength);
+
+			System.out.println("compresserOutput: " + Hex.encodeHexString(compresserOutput));
+			System.out.println("compactData: " + Hex.encodeHexString(compactData));
+
+//			Path path = Paths.get("string_de_teste_compactada.qzip");
+//			byte[] data = Files.readAllBytes(path);
+//			System.out.println("string_de_teste_compactada: " + Hex.encodeHexString(data));
 
 			// Decompress the bytes
 			Inflater decompresser = new Inflater();
-//			decompresser.setInput(output, 0, compressedDataLength);
-			decompresser.setInput(data, 0, data.length);
-			byte[] result = new byte[2048];
-			int resultLength = decompresser.inflate(result);
+			decompresser.setInput(compactData);
+			byte[] decompresserOutput = new byte[2048];
+			int decompresserOutputLength = decompresser.inflate(decompresserOutput);
 			decompresser.end();
+			byte[] decompressedData = Arrays.copyOfRange(decompresserOutput, 0, decompresserOutputLength);
 
 			// Decode the bytes into a String
-			String outputString = new String(result, 0, resultLength, "UTF-8");
-			System.out.println(outputString);
+			System.out.println("decompressedData: " + Hex.encodeHexString(decompressedData));
+			String outputString = new String(decompressedData);
+			System.out.println("String original: " + outputString);
 		} catch (java.io.UnsupportedEncodingException ex) {
 			ex.printStackTrace();
 		} catch (java.util.zip.DataFormatException ex) {
